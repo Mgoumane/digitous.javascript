233f67ab37d34074f12ef36eef67f0320c03351b
Mgoumane <goumane.mahmoudou@gmail.com>
2020-12-07 15:06:46 +0100

    I finished 05

--- jour01.js
+++ jour01.js
@@ -5,3 +5,14 @@ console.log("Hello World !")
 var test = "My name is Mahmoudou"
 console.log(test)
 
+// 03 - Concatenation
+var name = "Mahmoudou"
+console.log("Nice to meet you" + name)
+
+//04 - String Length
+var testLenght = "I'm very long !"
+console.log(testLenght)
+
+//05 - Replace
+var food = "croissant is meh"
+console.log(food.replace("meh", "so good"));

76ea74b18a1564d1f9f384058ebb1f09d79027e2
Mgoumane <goumane.mahmoudou@gmail.com>
2020-12-09 15:13:35 +0100

    i finished the exo 03

--- jour03.js
+++ jour03.js
@@ -3,4 +3,21 @@ var fruits = ["mango", "lemon", "blueberry"];
 console.table(fruits);
 console.log(fruits);
 
-// 02 - 
+// 02 - Acces
+
+var ingredients =["eggs", "milk", "butter"];
+console.log(ingredients[1]);
+console.log(ingredients.indexOf("butter"));
+
+// 03 - Add and Remove
+var objects = ["pen", "book", "lamp"];
+objects.unshift("chair");
+console.log(objects);
+objects.pop("lamp");
+console.log(objects);
+objects.push("laptop");
+console.log(objects);
+objects.shift("chair");
+console.log(objects);
+
+//04 -

07bdc4a67e40b7daa58597bc5548565b0885f9cd
Mgoumane <goumane.mahmoudou@gmail.com>
2020-12-14 15:22:32 +0100

    exo02 finished

--- jour04.js
+++ jour04.js
@@ -8,4 +8,14 @@ console.log(cat);
 console.log(cat.age);
 if (cat.isCute === true) {  
     console.log("So cute");
-}git 
+}
+
+// Exo - 02 
+var cat2 = {
+    name : "minou",
+    age : 4,
+    isCute : false,
+};
+var cats = [cat , cat2] ;
+console.log(cats[0].age );
+console.log(cat2.isCute);

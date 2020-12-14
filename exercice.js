145b3317662e50a30232caaac11a6cee3410d1a7
Mgoumane <goumane.mahmoudou@gmail.com>
2020-12-14 14:53:56 +0100

    i finished the exo 06

---
+++ exercice.js
@@ -0,0 +1,10 @@
+// Exercice 1: afficher la somme de tout les entier allant de 25 à 75
+
+var list = ["pain","beurre","cafe"]
+for(var i =0; i<list.length  ;i++){
+    var item= list[i];
+    if (item.charAt[0]==="b"){
+        console.log(item);
+    }
+    
+}

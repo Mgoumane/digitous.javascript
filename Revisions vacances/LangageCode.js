90973d382c4aee67231187db1eaeee44824bf64e
Mgoumane <goumane.mahmoudou@gmail.com>
2020-12-30 21:36:02 +0100

    30/12/2020

---
+++ Revisions vacances/LangageCode.js
@@ -0,0 +1,12 @@
+/*CONSIGNE :
+Créez une fonction secretCode qui prend une string en paramètre et qui remplace chaque lettre par leur ordre d'apparition dans l'alphabet + 10
+A devient 11, B devient 12, etc.
+Retournez le résultat sous forme de string
+
+console.log(secretCode("secret")); // affichera 291513281530 
+*/
+
+
+function secretCode(string){
+
+}
---
+++ Revisions vacances/countvowels.js
@@ -0,0 +1,30 @@
+function countvowels(string){
+    
+    console.log (string.length);
+}
+countvowels("mamoudou");
+
+
+
+var boite = "bonjour";
+var boiteEnTableau = boite.split("");
+var NombreDeBoiteEnTableau = boiteEnTableau.length;
+
+console.log(boite)
+console.log(NombreDeBoiteEnTableau)
+console.log(boiteEnTableau);
+
+
+// JE VEUX UNE BOUCLE QUI S ARRETE A NOMBREDEBOITENTABLEAU
+
+for(var i = 0; i< NombreDeBoiteEnTableau; i++){
+    var indexDeBoite = boiteEnTableau[i];
+    
+
+    
+}
+console.log(nombreDeVoyelles);
+
+console.log("mamoudou")
+
+

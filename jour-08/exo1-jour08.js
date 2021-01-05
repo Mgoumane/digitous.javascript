6b020f93d0fad60c380bf74e2297c0c1feff989c
Mgoumane <goumane.mahmoudou@gmail.com>
2021-01-05 17:12:04 +0100

    EX1

--- jour-08/exo1-jour08.js
+++ jour-08/exo1-jour08.js
@@ -7,12 +7,18 @@
 var request = require('request');
 
 var countriesNames = [];
-function getCountries(){
 
-    request.get("https://restcountries.eu/rest/v1/all", function (err, res, body) {
-    // je vais devoir faire une boucle qui va parcourir tout l'array BODY pour récuperer la valeur de la clé name dans chaque objet traversé
+
+request.get("https://restcountries.eu/rest/v1/all", function (err, res, body) {
     
-});
+    var countries =  JSON.parse(body);
+    
+    for(var i = 0 ; i < countries.length; i++){ 
+        countriesNames.push(countries[i].name);
+       
+        var countriesNamesAvectirets = countriesNames.join();
+        console.log(countriesNamesAvectirets);
+    }
 
-}
+});
 
---
+++ jour-08/exo2-jour08.js
@@ -0,0 +1,3 @@
+// 02 - Chuck Norris
+// À l'aide de l'api suivante (lisez bien la documentation) : https://api.chucknorris.io/
+// Créez une fonction asynchrone getFact qui récupèrera une blague Chuck Norris aléatoire à chaque fois que la fonction est lancée, et qui l'affichera dans la console
